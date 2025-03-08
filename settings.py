f555c70aac62e748fb44fa56b0e8cecc2c016cdd
AVECProductions <olheron@avecproductions.com>
2025-03-08 12:05:02 -0500

    added subscription

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
- 
+from decouple import config
+
+# ... other settings ...
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)  # Temporary debug line 
